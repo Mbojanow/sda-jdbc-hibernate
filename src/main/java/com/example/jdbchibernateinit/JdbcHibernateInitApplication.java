536529538a855fb2a9ffd3c5baa351bedb619f7d
Michal Bojanowski <bojanowski.michal89@gmail.com>
2021-04-24 18:42:29 +0200

    Initial commit

---
+++ src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
@@ -0,0 +1,10 @@
+package com.example.jdbchibernateinit;
+
+public class JdbcHibernateInitApplication {
+
+  public static void main(String[] args) {
+    var x = 3;
+    System.out.println("Hello");
+  }
+
+}
