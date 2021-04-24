218a35272a06e8b143cb3eb0c23bc744da55fa8b
Michal Bojanowski <bojanowski.michal89@gmail.com>
2021-04-24 19:24:53 +0200

    zad2

--- src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
+++ src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
@@ -1,14 +1,21 @@
 package com.example.jdbchibernateinit;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.h2.jdbcx.JdbcDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcHibernateInitApplication {
 
   public static void main(String[] args) {
-    try (final Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/information_schema",
-        "root", "example2")) {
+    MysqlDataSource dataSource = new MysqlDataSource();
+    dataSource.setUrl("jdbc:mysql://localhost:3306");
+    dataSource.setUser("root");
+    dataSource.setPassword("example");
+
+    try (final Connection connection = dataSource.getConnection()) {
+
     } catch (SQLException exp) {
       exp.printStackTrace();
     }
