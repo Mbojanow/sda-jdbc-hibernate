f6210595ca5a785a0eb38907cdbfc6f343b818f6
Michal Bojanowski <bojanowski.michal89@gmail.com>
2021-04-24 20:04:43 +0200

    zad4

--- src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
+++ src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
@@ -1,21 +1,28 @@
 package com.example.jdbchibernateinit;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import org.h2.jdbcx.JdbcDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcHibernateInitApplication {
 
   public static void main(String[] args) {
     MysqlDataSource dataSource = new MysqlDataSource();
-    dataSource.setUrl("jdbc:mysql://localhost:3306");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/sda_users");
     dataSource.setUser("root");
     dataSource.setPassword("example");
 
-    try (final Connection connection = dataSource.getConnection()) {
-
+    try (final Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      boolean execute = statement.execute("CREATE TABLE IF NOT EXISTS users(" +
+          "id INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+          "name VARCHAR(255) NOT NULL," +
+          "email VARCHAR(127) NOT NULL," +
+          "password VARCHAR(100) NOT NULL" +
+          ")");
+      System.out.println(execute);
     } catch (SQLException exp) {
       exp.printStackTrace();
     }
