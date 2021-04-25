3d56e5798cb5009ed6746819ebeeb3fc4865509c
Michal Bojanowski <bojanowski.michal89@gmail.com>
2021-04-25 12:20:09 +0200

    zadanie 3

--- src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
+++ src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
@@ -2,10 +2,12 @@ package com.example.jdbchibernateinit;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public class JdbcHibernateInitApplication {
 
@@ -16,11 +18,29 @@ public class JdbcHibernateInitApplication {
 
 
     try (Session session = sessionFactory.openSession()) {
-      NativeQuery<Object[]> nativeQuery = session.createNativeQuery("SELECT SYSDATE(), DATABASE()", Object[].class);
+      NativeQuery<Object[]> nativeQuery = session.createNativeQuery("SELECT SYSDATE(), DATABASE()");
       Object[] singleResult = nativeQuery.getSingleResult();
       Timestamp timestamp = (Timestamp)singleResult[0];
       String dbName = (String)singleResult[1];
       System.out.println(timestamp + " " + dbName);
+
+      Transaction transaction = session.beginTransaction();
+      NativeQuery insertQuery = session.createNativeQuery("INSERT INTO cars(manufacturer, model_name) VALUES (:manufacturer, :name)");
+      insertQuery.setParameter("manufacturer", "Toyota");
+      insertQuery.setParameter("name", "Corolla");
+      int rowsInserted = insertQuery.executeUpdate();
+
+
+      List<Object[]> resultList = session.createNativeQuery("SELECT * FROM cars").getResultList();
+      for (var row : resultList) {
+        System.out.println(row[0] + " " + row[1] + " " + row[2]);
+      }
+
+      if (rowsInserted == 1) {
+        transaction.commit();
+      } else {
+        transaction.rollback();
+      }
     }
   }
 
