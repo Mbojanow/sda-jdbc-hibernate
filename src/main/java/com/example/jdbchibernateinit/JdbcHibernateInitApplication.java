a1c1a514632fe86fd6388118783ce880b02adee6
Michal Bojanowski <bojanowski.michal89@gmail.com>
2021-04-25 12:03:56 +0200

    zadanie2

--- src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
+++ src/main/java/com/example/jdbchibernateinit/JdbcHibernateInitApplication.java
@@ -1,7 +1,11 @@
 package com.example.jdbchibernateinit;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
+
+import java.sql.Timestamp;
 
 public class JdbcHibernateInitApplication {
 
@@ -9,6 +13,15 @@ public class JdbcHibernateInitApplication {
     final SessionFactory sessionFactory = new Configuration()
         .configure("hibernate.cfg.xml")
         .buildSessionFactory();
+
+
+    try (Session session = sessionFactory.openSession()) {
+      NativeQuery<Object[]> nativeQuery = session.createNativeQuery("SELECT SYSDATE(), DATABASE()", Object[].class);
+      Object[] singleResult = nativeQuery.getSingleResult();
+      Timestamp timestamp = (Timestamp)singleResult[0];
+      String dbName = (String)singleResult[1];
+      System.out.println(timestamp + " " + dbName);
+    }
   }
 
 }
